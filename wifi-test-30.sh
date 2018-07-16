41b621007aeb4fe88be3ff5bc005cbe84aa3175b
idanless <31201417+idanless@users.noreply.github.com>
2018-07-16 10:07:27 +0300

    Update wifi-test-30.sh

--- wifi-test-30.sh
+++ wifi-test-30.sh
@@ -18,18 +18,48 @@ do
 sudo wpa_supplicant -B -i $int -c wpa_config.conf
 sleep 3
 echo $int
-sleep 15
+sleep 5
 done
 }
 
 
 dhcp() {
+iwconfig 2>&1 | grep ESSID | grep $ssid | cut -d ' ' -f1 >tmplist
 cat tmplist | while read int; 
 do
 echo runnig dhcp for $int
+ifconfig $int 0.0.0.0
+sleep 0.5
 dhclient -i $int
-sleep 5
+sleep 10
 done
+pkill dhclient
+
+}
+
+
+dhcplase() {
+	associate=$(iwconfig 2>&1 | grep ESSID | grep $ssid | wc -l)
+zenity --info --text "done up $num wifi \n associate success $associate"
+while true ;do
+    sleep 1500
+	dhclient
+	pkill dhclient
+done
+
+
+}
+
+
+
+dhcp2() {
+cat wifi-inter.txt | while read int; 
+do
+echo runnig clean for $int
+ifconfig $int 0.0.0.0
+sleep 0.5
+done
+dhclient -r
 }
 
 
@@ -45,7 +75,7 @@ done
 }
 
 VMrun() {
-for i in {1..31}
+for i in {1..30}
 do
    echo wifi-c$i
    VBoxManage startvm "wifi-c$i" --type headless
@@ -62,7 +92,9 @@ sudo /etc/init.d/network-manager start
 sleep 1
 sudo /etc/init.d/network-manager stop
 #dhclient -r
+sudo pkill dhclient
 sudo pkill wpa_supplicant
+dhcp2
 sleep 3
 ssid=$(zenity --entry --text "SSID name?" --entry-text "SSID")
 password=$(zenity --entry --text "password ssid?" --entry-text "password")
@@ -82,6 +114,7 @@ then
 	echo "run wifi full"
 	wififull1
 	dhcp
+	dhcplase
 
 elif [ $radio = VMwifi ]
 then
@@ -89,7 +122,7 @@ then
 	num=$(zenity --scale --text "pick a number" --min-value=1 --max-value=31 --value=1 --step 2)
 	VMwifi
 	echo "run vm"
-	su -c "bash 30vm.sh" -s /bin/sh "write user"
+	su -c "bash 30vm.sh" -s /bin/sh "$USER"
 	
 
 else
@@ -103,6 +136,3 @@ sleep 10
 #dhclient
 associate=$(iwconfig 2>&1 | grep ESSID | grep $ssid | wc -l)
 zenity --info --text "done up $num wifi \n associate success $associate"
-
-
-
